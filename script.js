c41691d0ee889db66cf5c2bfe28a8a199b6ea181
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-08 00:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+// Add click event listener to each panel
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    // Remove active class from all panels
+    panels.forEach(p => p.classList.remove('active'));
+    // Add active class to clicked panel
+    panel.classList.add('active');
+    // Make clicked panel wider
+    panel.style.flex = 5;
+    // Make other panels narrower
+    panels.forEach(p => {
+      if (p !== panel) {
+        p.style.flex = 1;
+      }
+    });
+  });
+});
+
